f1f54039c50adc16bf51a08e7c9408949491fcd5
mnsrulz <manishrawat4u@gmail.com>
2026-04-05 23:39:22 -0400

    Refactor main-options-weekly-consolidate.py to use TMP_DIR for output files and add file size logging

--- jobs/main-options-weekly-consolidate.py
+++ jobs/main-options-weekly-consolidate.py
@@ -1,11 +1,15 @@
 import os, uuid
+import shutil
 import pandas as pd
 
 MAX_SIZE = 20 * 1024 * 1024  # 20 MB in bytes
 CONSOLIDATED_DATA_DIR = os.environ.get("DATA_DIR")
+TMP_DIR = os.environ.get("TEMP_DIR")
 MIN_FILES_COUNT = int(os.environ.get("MIN_FILES_COUNT") or 5)
 if not CONSOLIDATED_DATA_DIR:
     raise ValueError(f"DATA_DIR env var is not set")
+if not TMP_DIR:
+    raise ValueError(f"TEMP_DIR env var is not set")
 if MIN_FILES_COUNT < 2:
     raise ValueError(f"MIN_FILES_COUNT should be at least 2")
 
@@ -33,11 +37,18 @@ for folder_name in os.listdir(CONSOLIDATED_DATA_DIR):
         combined_df = pd.concat(dfs, ignore_index=True)
 
         # Output file path (in the same folder)
-        output_file = os.path.join(folder_path, f"{uuid.uuid4()}.parquet")
+        output_file = os.path.join(TMP_DIR, f"{uuid.uuid4()}.parquet")
 
         # Save combined Parquet file
         combined_df.to_parquet(output_file, index=False)
-        print(f"Combined {symbol} {len(parquet_files)} files into {output_file}")
+        # list the size of teh file 
+        file_size_bytes = os.path.getsize(output_file) 
+        file_size_mb = file_size_bytes / (1024 * 1024)
+
+        print(f"Combined {symbol} {len(parquet_files)} files into {output_file} ({file_size_mb:.2f} MB)")
+        
+        # Move the consolidated file back to the original folder, overwriting if it already exists
+        shutil.move(output_file, folder_path)
 
         # Delete original files one by one
         for pf in parquet_files:
