4a4dc33d2c24632777a94686e1191c21d04fb336
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 19:18:42 -0400

    using local download

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,9 +2,6 @@
 import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.32.0/dist/duckdb-browser-blocking.mjs/+esm';
 import { Hono } from "https://esm.sh/hono@4.12";
 import { handle } from "https://esm.sh/hono@4.12/netlify";
-import data from "../../public/options_data.parquet" with {
-    type: "bytes",
-};
 
 const logger = new ConsoleLogger();
 const JSDELIVR_BUNDLES = getJsDelivrBundles();
@@ -13,15 +10,19 @@ await db.instantiate(() => { });
 
 const app = new Hono();
 
+const url = new URL("/options_data.parquet", import.meta.url);
+const res = await fetch(url);
+const buffer = await res.arrayBuffer();
+
 app.get("/api/hello", (c) => {
   const connection = db.connect();
-  const t = Deno.readDirSync("public");
+  
 
   //const connection = await duckDbInstance.connect();
   const result = connection.query(`SELECT version() AS version`);
 
   const rows = result.toArray();
-  return c.json({ message: "Hello from Deno on Netlify Edge!", rows, files: t.map(k => k.name), d: data.length });
+  return c.json({ message: "Hello from Deno on Netlify Edge!", rows, d: buffer.byteLength });
 });
 
 app.get("/api/query", (c) => {
