80f7e0cb230460f323d14fd39307b475214bc12c
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 18:37:08 -0400

    using 1.32 version

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -1,5 +1,5 @@
-// @deno-types="https://esm.sh/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.d.ts"
-import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
+// @deno-types="https://esm.sh/@duckdb/duckdb-wasm@1.32.0/dist/duckdb-browser-blocking.d.ts"
+import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.32.0/dist/duckdb-browser-blocking.mjs/+esm';
 import { Hono } from "hono";
 import { handle } from "hono/netlify";
 
@@ -11,11 +11,11 @@ await db.instantiate(() => { });
 const app = new Hono();
 
 
-app.get("/api/hello", async (c) => {
-  const connection = await db.connect();
+app.get("/api/hello", (c) => {
+  const connection = db.connect();
 
   //const connection = await duckDbInstance.connect();
-  const result = await connection.query(`SELECT 1 AS value`);
+  const result = connection.query(`SELECT version() AS version`);
   
   const rows = result.toArray();
   return c.json({ message: "Hello from Deno on Netlify Edge!", rows });
