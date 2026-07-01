82485b38b700743550c864da74d1aedfaa849b53
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 18:08:55 -0400

    add duckdb

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import { handle } from "hono/netlify";
+import { DuckDBInstance } from "@duckdb/node-api";
 
 const app = new Hono();
+const duckDbInstance = await DuckDBInstance.create(":memory:");
 
-app.get("/api/hello", (c) => {
-  return c.json({ message: "Hello from Deno on Netlify Edge!" });
+app.get("/api/hello", async (c) => {
+  const connection = await duckDbInstance.connect();
+  const result = await connection.runAndReadAll(`SELECT 1 AS value`)
+  const rows = { rows: result.getRowsJson(), columns: result.columnNamesAndTypesJson() };
+  return c.json({ message: "Hello from Deno on Netlify Edge!", rows });
 });
 
 export const config = { path: "/api/*" };
