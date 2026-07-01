a387118921aa8dd4ed0149051258300ba4e512dd
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 18:25:34 -0400

    fix wasm

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -1,14 +1,29 @@
 import { Hono } from "hono";
 import { handle } from "hono/netlify";
-import { DuckDBInstance } from "https://esm.sh/@duckdb/duckdb-wasm";
+import * as duckdb from "https://esm.sh/@duckdb/duckdb-wasm";
 
 const app = new Hono();
-const duckDbInstance = await DuckDBInstance.create(":memory:");
+
 
 app.get("/api/hello", async (c) => {
-  const connection = await duckDbInstance.connect();
-  const result = await connection.runAndReadAll(`SELECT 1 AS value`)
-  const rows = { rows: result.getRowsJson(), columns: result.columnNamesAndTypesJson() };
+
+  const bundles = duckdb.getJsDelivrBundles();
+  const bundle = await duckdb.selectBundle(bundles);
+
+  const worker = new Worker(bundle.mainWorker!);
+
+  const logger = new duckdb.ConsoleLogger();
+
+  const db = new duckdb.AsyncDuckDB(logger, worker);
+
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+  const connection = await db.connect();
+
+  //const connection = await duckDbInstance.connect();
+  const result = await connection.query(`SELECT 1 AS value`);
+  
+  const rows = result.toArray();
   return c.json({ message: "Hello from Deno on Netlify Edge!", rows });
 });
 
