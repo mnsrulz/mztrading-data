d989e5e6a44634e90fd74c0fe919c5ba940a1e9a
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 21:55:17 -0400

    error handling

--- netlify/edge-functions/api4.ts
+++ netlify/edge-functions/api4.ts
@@ -4,14 +4,18 @@ import { walk } from "https://deno.land/std@0.170.0/fs/walk.ts";
 export default async (request: Request, context: Context) => {
   const files = Deno.readDirSync("./");//.map(({name, isDirectory, isFile, isSymlink}) => ({ name, isDirectory, isFile, isSymlink }));
   const fileList = []
-  for await (const walkEntry of walk("./")) {
-    const type = walkEntry.isSymlink
+  try {
+    for await (const walkEntry of walk("./")) {
+      const type = walkEntry.isSymlink
       ? "symlink"
       : walkEntry.isFile
       ? "file"
       : "directory";
-
-    fileList.push({type, path: walkEntry.path});
+      
+      fileList.push({type, path: walkEntry.path});
+    }
+  } catch (error) {
+    console.error("Error walking the directory:", error);
   }
   return Response.json({
     files: Array.from(files), 
