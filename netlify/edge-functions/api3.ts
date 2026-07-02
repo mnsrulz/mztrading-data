fa87abb2ea11242a94d75e948ed613fe345ec1f1
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 22:18:29 -0400

    use bytes

--- netlify/edge-functions/api3.ts
+++ netlify/edge-functions/api3.ts
@@ -2,10 +2,15 @@ import { allTickerSymbols } from "../../lib/data.ts"
 import { Hono } from "hono";
 import { handle } from "hono/netlify";
 
+import p from "./../../public/options_data.parquet" with {
+    type: "bytes",
+};
+
 const app = new Hono();
 app.get("/api3/hello", (c) => {
     return c.json({
-        symbols: allTickerSymbols
+        symbols: allTickerSymbols,
+        l: p.length
     });
 });
 
