d1f4210e01161de5c0deee7c10937d6302bc1404
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 20:49:45 -0400

    use api2

--- netlify/edge-functions/api2.ts
+++ netlify/edge-functions/api2.ts
@@ -3,7 +3,7 @@ import { Hono } from "https://esm.sh/hono@4.12";
 import { handle } from "https://esm.sh/hono@4.12/netlify";
 
 const app = new Hono();
-app.get("/api/hello", (c) => {
+app.get("/api2/hello", (c) => {
     const symbol = c.req.query("symbol") || "AAPL";
     const expiry = c.req.query("expiry") || "2026-01-16";
     return c.json({
