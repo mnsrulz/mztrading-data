0e453aa58188594f832bc3cd22ec1dd1314fab48
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 20:47:32 -0400

    add api2

---
+++ netlify/edge-functions/api2.ts
@@ -0,0 +1,17 @@
+import { OptionsExpirationStrikes } from "../../lib/data.ts"
+import { Hono } from "https://esm.sh/hono@4.12";
+import { handle } from "https://esm.sh/hono@4.12/netlify";
+
+const app = new Hono();
+app.get("/api/hello", (c) => {
+    const symbol = c.req.query("symbol") || "AAPL";
+    const expiry = c.req.query("expiry") || "2026-01-16";
+    return c.json({
+        symbol,
+        expiry,
+        data: OptionsExpirationStrikes[symbol][expiry]
+    });
+});
+
+export const config = { path: "/api2/*" };
+export default handle(app);
