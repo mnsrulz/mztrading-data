ea90362da3d10ff5788e05560f1984b7f26b84d3
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 21:52:56 -0400

    use walk

--- netlify/edge-functions/api4.ts
+++ netlify/edge-functions/api4.ts
@@ -1,9 +1,21 @@
 import type { Context } from "@netlify/edge-functions";
+import { walk } from "https://deno.land/std@0.170.0/fs/walk.ts";
 
 export default async (request: Request, context: Context) => {
   const files = Deno.readDirSync("./");//.map(({name, isDirectory, isFile, isSymlink}) => ({ name, isDirectory, isFile, isSymlink }));
+  const fileList = []
+  for await (const walkEntry of walk("./")) {
+    const type = walkEntry.isSymlink
+      ? "symlink"
+      : walkEntry.isFile
+      ? "file"
+      : "directory";
+
+    fileList.push({type, path: walkEntry.path});
+  }
   return Response.json({
-    files: Array.from(files)
+    files: Array.from(files), 
+    fileList,
   }, { status: 200 });
 };
 
