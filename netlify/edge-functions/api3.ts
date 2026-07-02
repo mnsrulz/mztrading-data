d01303618ca389808d7164bd8d6f95a7d1d7eec7
mnsrulz <manishrawat4u@gmail.com>
2026-07-01 22:57:55 -0400

    rmeove import

--- netlify/edge-functions/api3.ts
+++ netlify/edge-functions/api3.ts
@@ -2,15 +2,11 @@ import { allTickerSymbols } from "../../lib/data.ts"
 import { Hono } from "https://esm.sh/hono@4.12";
 import { handle } from "https://esm.sh/hono@4.12/netlify";
 
-import p from "./../../public/options_data.parquet" with {
-    type: "bytes",
-};
 
 const app = new Hono();
 app.get("/api3/hello", (c) => {
     return c.json({
         symbols: allTickerSymbols,
-        l: p.length
     });
 });
 
