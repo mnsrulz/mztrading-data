558c243f255eb022399225827646fcb365038839
mnsrulz <manishrawat4u@gmail.com>
2026-07-17 00:43:27 -0400

    fix build

--- lib/historicalPrice.ts
+++ lib/historicalPrice.ts
@@ -14,7 +14,7 @@ export async function getPriceAtDate(
 ): Promise<string | undefined | null>;
 
 export async function getPriceAtDate(symbol: string, dt: string, fallbackToPreviousDayWhenNoPriceFound: boolean, keepOriginalValue = true) {
-    const { price } = await ky('https://live-quotes.mztrading.workers.dev/price?s=AAPL&dt=2026-07-16', {
+    const { price } = await ky('https://live-quotes.mztrading.workers.dev/price', {
         searchParams: {
             s: symbol,
             dt: dt,
@@ -25,13 +25,19 @@ export async function getPriceAtDate(symbol: string, dt: string, fallbackToPrevi
     return price;
 }
 
-// export const getLastNPrices = async (symbol: string, lastN: number, interval: 'd' | 'h') => {
-//     const t = Math.ceil(interval == 'h' ? Math.ceil((lastN * 1.2) / 40) : Math.ceil((lastN * 1.2) / 5));       //take extra couple days of data just to be sure we have enough data
-//     const start = dayjs().format('YYYY-MM-DD');
-//     const resp = await yf.chart(EXCEPTION_SYMBOLS[symbol.toUpperCase()] || symbol, {
-//         interval: interval == 'd' ? '1d' : '1h',
-//         period1: dayjs(start).add(-t, 'week').toDate(),
-//         period2: dayjs(start).toDate()
-//     })
-//     return resp.quotes.map(({ close, date }) => ({ close, date })).filter(k => k.close != null).map(({ close, date }) => ({ date, close: Number(close) })).slice(-lastN);
-// }
+export const getLastNPrices = async (symbol: string, lastN: number, interval: 'd' | 'h') => {
+    const { prices } = await ky('https://live-quotes.mztrading.workers.dev/prices', {
+        searchParams: {
+            s: symbol,
+            n: lastN,
+            i: interval
+        }
+    }).json<{
+        prices: {
+            date: Date,
+            close: number
+        }[]
+    }>();
+
+    return prices;
+}
