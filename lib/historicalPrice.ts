fc301412e43b2e859d1e9d01439f4461ae639933
mnsrulz <manishrawat4u@gmail.com>
2026-07-17 00:34:07 -0400

    using external api for historical price

--- lib/historicalPrice.ts
+++ lib/historicalPrice.ts
@@ -1,58 +1,37 @@
-import dayjs from "https://esm.sh/dayjs@1.11.13";
-import utc from "https://esm.sh/dayjs@1.11.13/plugin/utc";
-import isToday from "https://esm.sh/dayjs@1.11.13/plugin/isToday";
-import timezone from "https://esm.sh/dayjs@1.11.13/plugin/timezone";
-
-import yf from 'npm:yahoo-finance2@2.11.3';
-
-import "https://esm.sh/dayjs@1.11.13/locale/en";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(isToday);
-
-const EXCEPTION_SYMBOLS = {
-    'SPX': '^SPX',
-    'VIX': '^VIX',
-} as Record<string, string>
-
+import ky from 'https://esm.sh/ky@1.8.2';
 export async function getPriceAtDate(
-  symbol: string,
-  dt: string,
-  fallbackToPreviousDayWhenNoPriceFound: boolean,
-  keepOriginalValue: true,
+    symbol: string,
+    dt: string,
+    fallbackToPreviousDayWhenNoPriceFound: boolean,
+    keepOriginalValue: true,
 ): Promise<number | undefined | null>;
 
 export async function getPriceAtDate(
-  symbol: string,
-  dt: string,
-  fallbackToPreviousDayWhenNoPriceFound: boolean,
-  keepOriginalValue?: false
+    symbol: string,
+    dt: string,
+    fallbackToPreviousDayWhenNoPriceFound: boolean,
+    keepOriginalValue?: false
 ): Promise<string | undefined | null>;
 
 export async function getPriceAtDate(symbol: string, dt: string, fallbackToPreviousDayWhenNoPriceFound: boolean, keepOriginalValue = true) {
-    try {
-        const start = dayjs(dt.substring(0, 10)).format('YYYY-MM-DD');
-        const resp = await yf.chart(EXCEPTION_SYMBOLS[symbol.toUpperCase()] || symbol, {
-            interval: '1d',
-            period1: dayjs(start).add(-7, 'day').toDate(),
-            period2: dayjs(start).toDate()
-        })
-        const priceToReturn = fallbackToPreviousDayWhenNoPriceFound ? resp.quotes.reverse().find(k=> k.close != null)?.close : resp.quotes.at(-1)?.close;
-        return keepOriginalValue ? priceToReturn : priceToReturn?.toFixed(2);
-    } catch (error) {
-        console.error(`Error fetching price for ${symbol} on ${dt}:`, error);
-        return null;
-    }
+    const { price } = await ky('https://live-quotes.mztrading.workers.dev/price?s=AAPL&dt=2026-07-16', {
+        searchParams: {
+            s: symbol,
+            dt: dt,
+            f: fallbackToPreviousDayWhenNoPriceFound ? '1' : '0',
+            o: keepOriginalValue ? '1' : '0'
+        }
+    }).json<{ price?: number | string | null }>();
+    return price;
 }
 
-export const getLastNPrices = async (symbol: string, lastN: number, interval: 'd' | 'h') => {
-    const t = Math.ceil(interval == 'h' ? Math.ceil((lastN * 1.2) / 40) : Math.ceil((lastN * 1.2) / 5));       //take extra couple days of data just to be sure we have enough data
-    const start = dayjs().format('YYYY-MM-DD');
-    const resp = await yf.chart(EXCEPTION_SYMBOLS[symbol.toUpperCase()] || symbol, {
-        interval: interval == 'd' ? '1d' : '1h',
-        period1: dayjs(start).add(-t, 'week').toDate(),
-        period2: dayjs(start).toDate()
-    })
-    return resp.quotes.map(({ close, date }) => ({ close, date })).filter(k => k.close != null).map(({ close, date }) => ({ date, close: Number(close) })).slice(-lastN);
-}
+// export const getLastNPrices = async (symbol: string, lastN: number, interval: 'd' | 'h') => {
+//     const t = Math.ceil(interval == 'h' ? Math.ceil((lastN * 1.2) / 40) : Math.ceil((lastN * 1.2) / 5));       //take extra couple days of data just to be sure we have enough data
+//     const start = dayjs().format('YYYY-MM-DD');
+//     const resp = await yf.chart(EXCEPTION_SYMBOLS[symbol.toUpperCase()] || symbol, {
+//         interval: interval == 'd' ? '1d' : '1h',
+//         period1: dayjs(start).add(-t, 'week').toDate(),
+//         period2: dayjs(start).toDate()
+//     })
+//     return resp.quotes.map(({ close, date }) => ({ close, date })).filter(k => k.close != null).map(({ close, date }) => ({ date, close: Number(close) })).slice(-lastN);
+// }
